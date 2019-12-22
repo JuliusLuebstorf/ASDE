f918c0363a45c6f632623e1e90fecab703cd16a8
Buonanno <grasta4@gmail.com>
2019-12-22 01:01:23 +0100

    Created AI

--- proj4/src/main/java/asde/proj4/games/tictactoe/Grid.java
+++ proj4/src/main/java/asde/proj4/games/tictactoe/Grid.java
@@ -1,14 +1,17 @@
 package asde.proj4.games.tictactoe;
 
 public class Grid {
-	public static final int COLUMNS = 3, ROWS = 3, SIZE = ROWS * COLUMNS;
-	private final Square[][] grid = new Square[ROWS][COLUMNS];
+	public static final int COLUMNS = 3, ROWS = 3, SIZE = ROWS * COLUMNS, NO_DIAGONALS = 2;
+	public static enum Direction { ROW, COLUMN, DIAGONAL };
+	private final Symbol[][] grid = new Symbol[ROWS][COLUMNS];
 	
 	public Grid() {
-		
+		for(int row = 0; row < ROWS; row++)
+			for(int column = 0; column < COLUMNS; column++)
+				grid[row][column] = Symbol.EMPTY;
 	}
 	
-	public Square[][] getGrid() {
+	public Symbol[][] getGrid() {
 		return grid;
 	}
 	
@@ -16,23 +19,61 @@ public class Grid {
 		return row >= 0 && row < ROWS && column >= 0 && column < COLUMNS;
 	}
 	
-	public Square getSquare(final int row, final int column) {
-		if(!validIndexes(row, column)) {
-			System.err.println("Invalid indexes: " + row + " " + column + "; returning null...");
-			
-			return null;
-		}
+	public Symbol get(final int row, final int column) {
+		if(!validIndexes(row, column))
+			throw new IllegalArgumentException("Invalid indexes: " + row + " " + column);
 		
 		return grid[row][column];
 	}
 	
-	public void setSquare(final int row, final int column, final Square square) {
-		if(!validIndexes(row, column)) {
-			System.err.println("Invalid indexes: " + row + " " + column + "; returning...");
+	public void set(final int row, final int column, final Symbol symbol) {
+		if(!validIndexes(row, column))
+			throw new IllegalArgumentException("Invalid indexes: " + row + " " + column);
+		
+		grid[row][column] = symbol;
+	}
+	
+	public boolean emptySquareAvailable() {
+		for(int row = 0; row < ROWS; row++)
+			for(int column = 0; column < COLUMNS; column++)
+				if(grid[row][column]== Symbol.EMPTY)
+					return true;
+		
+		return false;
+	}
+	
+	public boolean victory(final Symbol symbol, final Direction direction, final int start) {
+		if(((direction == Direction.ROW || direction == Direction.COLUMN) && (start < 0 || start >= ROWS)) || (direction == Direction.DIAGONAL && (start < 0 || start >= NO_DIAGONALS)))
+			throw new IllegalArgumentException("Invalid parameters: " + symbol + " " + direction + " " + start);
+		
+		int row = direction == Direction.ROW ? start : 0, column = direction == Direction.COLUMN ? start : (direction == Direction.DIAGONAL && start == 1) ? COLUMNS - 1 : 0;
+		
+		while(validIndexes(row, column)) {
+			if(grid[row][column] != symbol)
+				return false;
 			
-			return;
+			row = direction == Direction.ROW ? row : row + 1;
+			column = direction == Direction.COLUMN ? column : (direction == Direction.DIAGONAL && start == 1) ? column - 1 : column + 1;
 		}
 		
-		grid[row][column] = square;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		String string = "";
+		
+		for(int row = 0; row < ROWS; row++) {
+			for(int column = 0; column < COLUMNS; column++)
+				switch(grid[row][column]) {
+					case CIRCLE: string += 'o'; break;
+					case CROSS: string += 'x'; break;
+					case EMPTY: string += ' '; break;
+				}
+				
+			string += '\n';
+		}
+		
+		return string;
 	}
 }
