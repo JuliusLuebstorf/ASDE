699a438a8923ff5b3d6436224957d38383d08641
Adrian <45540128+averanes@users.noreply.github.com>
2020-01-15 16:56:51 +0100

    spring securiti ok loging and validation in the frontend and backend ok

--- proj4/src/main/java/asde/proj4/security/dao/IPersonaRepo.java
+++
@@ -1,9 +0,0 @@
-package asde.proj4.security.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import asde.proj4.security.domain.Persona;
-
-public interface IPersonaRepo extends JpaRepository<Persona, Integer>{
-	
-}
--- proj4/src/test/java/asde/proj4/UserTest.java
+++ proj4/src/test/java/asde/proj4/UserTest.java
@@ -12,6 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import asde.proj4.security.dao.UserPlayerDAO;
 import asde.proj4.security.domain.UserPlayer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 @RunWith(SpringRunner.class)
@@ -20,12 +27,11 @@ public class UserTest {
 	@Autowired
 	private UserPlayerDAO repository;
 	
+	/*
 	@Test
 	public void contextLoads() {
 		
 		repository.save(new UserPlayer("pp", "pp", "pp"));
-		
-		;
 		
 		assertEquals("pp", repository.findByUsernameOrEmail("aa", "pp").getUsername());
 		
@@ -35,5 +41,72 @@ public class UserTest {
 		assertEquals("pp", repository.findByUsernameOrEmail("aa", "aa").getUsername());
 		
 		repository.deleteAll();
+	}*/
+	
+	
+	@Test
+	public void loging() {
+		
+		String user = "";
+		try {
+
+			URL url = new URL("http://localhost:8080/perform_login");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+			String input = "{\"username\":\"a\",\"password\":\"a\"}";
+
+			OutputStream os = conn.getOutputStream();
+			os.write(input.getBytes());
+			os.flush();
+
+	                String token = conn.getHeaderField("Authorization");
+
+	                System.out.println(token.trim());
+
+			conn.disconnect();
+
+	                url = new URL("http://localhost:8080/currentUserName");
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+	                conn.setRequestProperty("Authorization", token);
+			conn.setRequestProperty("Accept", "application/json");
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from Server .... \n");
+			while ((output = br.readLine()) != null) {
+				user += output;
+				System.out.println(output);
+			}
+
+			
+			
+			conn.disconnect();                
+
+		  } catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		  } catch (IOException e) {
+
+			e.printStackTrace();
+
+		 }
+		
+		
+		
+		assertEquals("a", user);
+		
+		
 	}
 }
